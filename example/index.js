28c0c09b21aede18ce88520f70f5bc915bad17d6
xrsphp <735819938@qq.com>
2024-03-12 08:55:59 +0800

    modify example/index.js

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@
 import { exampleRequestGet } from './api.js';
 const example = async () => {
     let res = await exampleRequestGet();
-    console.log('请求成功 ');
+    console.log('请求成功!');
 };
 example();
 
